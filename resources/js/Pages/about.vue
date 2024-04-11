f6e20476e94996bdcae096a129f2ff46fff5cccd
nizaamomer <nizaamomer@gmail.com>
2024-04-11 23:22:54 +0300

    defult layout

--- resources/js/Pages/about.vue
+++ resources/js/Pages/about.vue
@@ -1,11 +1,3 @@
 <template>
     <div>about page</div>
 </template>
-<script setup>
-import Layout from "@/Layouts/layout.vue";
-</script>
-<script>
-export default {
-    layout: Layout,
-};
-</script>
